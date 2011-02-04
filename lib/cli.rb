5469e60889b7e52703fd2fedabf089defd045dec
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2011-02-04 22:19:52 +0100

    Removed knowledge of EC2 from the CLI.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,12 +1,9 @@
 require 'rubygems'
 require 'thor'
-require File.expand_path(File.join(File.dirname(__FILE__), 'providers/ec2.rb'))
 
 module TestbotCloud
   class Cli < Thor
     include Thor::Actions
-   
-    DEFAULT_PROVIDER = :ec2
 
     def self.providers=(providers)
       @@providers = providers
@@ -18,7 +15,7 @@ module TestbotCloud
 
     desc "new PROJECT_NAME", "Generate a testbot cloud project"
     def new(name)
-      @@providers[DEFAULT_PROVIDER].new(self).generate_project(name)
+      @@providers[TestbotCloud::DEFAULT_PROVIDER].new(self).generate_project(name)
     end
   end
 end
--- lib/testbot_cloud.rb
+++ lib/testbot_cloud.rb
@@ -1,5 +1,6 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'cli.rb'))
 
 module TestbotCloud
+  DEFAULT_PROVIDER = :ec2
 end
 
