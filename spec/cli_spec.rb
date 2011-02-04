617a77d0608582db116a1c7ceb7fabbfaacf48e4
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2011-02-04 22:29:46 +0100

    Removing some require duplication.

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,5 +1,4 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '../lib/testbot_cloud.rb'))
-require 'minitest/autorun'
+require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper.rb'))
 
 describe TestbotCloud::Cli do
 
