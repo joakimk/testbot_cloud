58d4edf70d988b16c19a0a988a3d98b8b94fd0e6
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2011-04-11 20:13:31 +0200

    Added aws support.

--- lib/server/aws.rb
+++ lib/server/aws.rb
@@ -6,8 +6,35 @@ module TestbotCloud
       end
 
       def bootstrap!(mutex)
-        true
+        unless ENV['INTEGRATION_TEST']
+          if online?(@server.dns_name)
+            return system("scp -o StrictHostKeyChecking=no -i testbot.pem -r bootstrap ubuntu@#{@server.dns_name}:~ &> /dev/null") &&
+                   system("ssh -o StrictHostKeyChecking=no -i testbot.pem ubuntu@#{@server.dns_name} 'cd bootstrap; sudo sh runner.sh' &> /dev/null")
+          end
+        else
+          return true
+        end
+
+        false
       end
+
+      private
+
+      def online?(ip)
+        # Wait a while, seems the server gets the ssh key some time after being ready.
+        sleep 5
+
+        10.times do
+          if system("ssh -o StrictHostKeyChecking=no -i testbot.pem ubuntu@#{ip} 'true' &> /dev/null")
+            return true
+          else
+            puts "#{@server.id} ssh connection failed, retrying..."
+            sleep 3
+          end
+        end
+
+        false
+      end      
     end
   end
 end
