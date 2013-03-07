23989446bc2905b46cd56f2d8776eced0682845f
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2013-03-07 10:54:39 +0100

    Update fog and support even newer fog.

--- spec/server/factory_spec.rb
+++ spec/server/factory_spec.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper.rb'))
 
 describe TestbotCloud::Server::Factory do
-  
   it "should create a server wrapper for Brightbox" do
     server = TestbotCloud::Server::Factory.create(nil, nil, Fog::Brightbox::Compute::Server.new)
     server.should be_instance_of(TestbotCloud::Server::Brightbox)
@@ -14,9 +13,7 @@ describe TestbotCloud::Server::Factory do
 
   it "should raise an error when there is no server wrapper" do
     expect {
-      TestbotCloud::Server::Factory.create(nil, nil, Fog::Slicehost::Compute::Server.new)
+      TestbotCloud::Server::Factory.create(nil, nil, Fog::Bluebox::Compute::Server.new)
     }.to raise_error(/Unsupported server type: /)
   end
-
-
 end
